c2c9148221610f6a22eb08d9e8a8f611dfd17149
zer0def <zer0def@zer0def.0>
2024-04-08 11:46:14 +0200

    entertain multi-trunked elephants

--- postgres-appliance/scripts/spilo_commons.py
+++ postgres-appliance/scripts/spilo_commons.py
@@ -12,10 +12,14 @@ LIB_DIR = '/usr/lib/postgresql'
 
 # (min_version, max_version, shared_preload_libraries, extwlist.extensions)
 extensions = {
+    #'columnar':       (13, 16, True,  True),
     'timescaledb':    (9.6, 17, True,  True),
     'pg_cron':        (9.5, 17, True,  False),
     'pg_stat_kcache': (9.4, 17, True,  False),
     'pg_partman':     (9.4, 17, False, True)
+    #'wrappers':       (14, 16, False, True),
+    #'pgsodium':       (14, 16, False, True),
+    #'supabase_vault': (14, 16, False, True),
 }
 if os.environ.get('ENABLE_PG_MON') == 'true':
     extensions['pg_mon'] = (11,  17, True,  False)
